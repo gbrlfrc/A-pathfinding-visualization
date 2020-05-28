1b88be10a0295969f4fb9ddfa573f1c43c06d74f
gabriele <61314427+gbrlfrc@users.noreply.github.com>
2020-05-28 16:00:15 +0000

    Update A_star_visualization.py

--- A_star_visualization.py
+++ A_star_visualization.py
@@ -5,15 +5,15 @@ import time
 WIN_SIZE=(700, 700)
 BLACK = ( 0, 0, 0)
 WHITE = ( 255, 255, 255)
-BLUE = ( 0, 0, 255)
+PATH = (0, 0, 102)
 RED = ( 255, 0, 0)
 GREEN = (0, 255, 0)
 CELL_SIZE=20
 
 pygame.init()
 class Cell:
-    def __init__(self, posx, posy, size, color, id, walkable, G, H, F, parent, outline):
-        self.posx = posx    #both posx and posy are the coordinates 
+    def __init__(self, posx, posy, size, color, walkable, G, H, F, parent, outline):
+        self.posx = posx    #both posx and posy are the coordinates
         self.posy = posy    #of a cell in pixel
         self.size = size
         self.color = color
@@ -26,11 +26,8 @@ class Cell:
 
 
     def setColor(self, color): self.color = color
-    def setWalkable(self, walkable): self.walkable=walkable #
-    def ifWalkable(self): return self.walkable
-    def setOutline(self): self.outline=outline
-    def draw(self, screen, outline):
-        pygame.draw.rect(screen, self.color, (self.posx, self.posy, self.size, self.size), outline)
+    def setWalkable(self, walkable): self.walkable=walkable
+    def setOutline(self, outline): self.outline=outline
     def getPosition(self): return (self.posx, self.posy)
 
 
@@ -38,9 +35,9 @@ def Astar(s, e, grid, screen):
     open_lst=close_lst=deque()
     open_lst.append(grid[s[0]][s[1]])
     while(len(open_lst)!=0):
+        time=0
         open_lst=deque(sorted(open_lst, key=lambda x: x.F))
         current=open_lst.popleft()
-
         if current==grid[e[0]][e[1]]:
             getPath(current, grid, screen)
             return    #path found
@@ -49,7 +46,7 @@ def Astar(s, e, grid, screen):
             currentGridPos=getGridPos(current.getPosition()[0], current.getPosition()[1])
             neighbors=getNeighbors(currentGridPos, grid)
             for neig in neighbors:
-                if(neig.ifWalkable()==True):
+                if(neig.walkable==True):
                     if(neig in open_lst and getCost(s[0], s[1], currentGridPos[0], currentGridPos[1])+current.G < neig.G):
                         neig.G=getCost(s[0], s[1], currentGridPos[0], currentGridPos[1])+current.G
                         neig.F=neig.G+neig.H
@@ -62,14 +59,17 @@ def Astar(s, e, grid, screen):
                         neig.parent=currentGridPos
                         open_lst.append(neig)
                         grid[neigGridPos[0]][neigGridPos[1]].setColor(GREEN)
-                        grid[neigGridPos[0]][neigGridPos[1]].outline=0
+                        # while(time%180!=0):time+=1
+                        # updateGrid(screen, grid)
     return
 
 def getPath(e, grid, screen):
-    if(e.parent[0] == -1): return
+    if(e.parent[0] == -1):
+        e.setColor(GREEN)
+        return
     else:
-        grid[e.parent[0]][e.parent[1]].draw(screen, 0)
-        e.setColor(BLUE)
+        grid[e.parent[0]][e.parent[1]].setOutline(0)
+        grid[e.parent[0]][e.parent[1]].setColor(PATH)
         getPath(grid[e.parent[0]][e.parent[1]], grid, screen)
 
 def getNeighbors(current_pos, grid):
@@ -106,47 +106,51 @@ def getGridPos(posx, posy):
     return (int(math.floor(posx/CELL_SIZE)), int(math.floor(posy/CELL_SIZE)))
 
 #update color and outline of the cells before update s
-def updateGrid(screen, grid, wall_pos):
+def updateGrid(screen, grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if wall_pos==(i, j): grid[i][j].draw(screen, 0)
-            else: grid[i][j].draw(screen, 1)
+            if grid[i][j].walkable==False:grid[i][j].setOutline(0)
+            pygame.draw.rect(screen, grid[i][j].color, (grid[i][j].posx, grid[i][j].posy, grid[i][j].size, grid[i][j].size), grid[i][j].outline)
     return grid
 
 # draw start cell, end cell and wall on grid
 def prepareCell(grid, screen, color, type):
     posInGrid=getGridPos(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
     if(type[2]==1):
-        grid[posInGrid[0]][posInGrid[1]].setWalkable(False)
+        grid[posInGrid[0]][posInGrid[1]].walkable=False
         grid[posInGrid[0]][posInGrid[1]].setColor(color)
-    if(type[1]==1): grid[posInGrid[0]][posInGrid[1]].setColor(color)
-    if(type[0]==1): grid[posInGrid[0]][posInGrid[1]].setColor(color)
+        grid[posInGrid[0]][posInGrid[1]].setOutline(1)
+    if(type[1]==1):
+        grid[posInGrid[0]][posInGrid[1]].walkable=True
+        grid[posInGrid[0]][posInGrid[1]].setColor(color)
+        grid[posInGrid[0]][posInGrid[1]].setOutline(0)
+
+    if(type[0]==1):
+        grid[posInGrid[0]][posInGrid[1]].walkable=True
+        grid[posInGrid[0]][posInGrid[1]].setColor(color)
+        grid[posInGrid[0]][posInGrid[1]].setOutline(0)
     return posInGrid
 
 
 def main():
     win_size=WIN_SIZE
     cell_size=CELL_SIZE
-    white, black, blue, red=WHITE, BLACK, BLUE, RED
+    white, black, blue, red, green=WHITE, BLACK, PATH, RED, GREEN
     wall_pos, start_pos, end_pos=(), (), ()
-    walls_lst=[]
     se=1
 
     screen=pygame.display.set_mode(win_size)
     pygame.display.set_caption("A* Visualization")
     screen.fill(white)
 
-    id=1
     grid=[]
     for i in range(0, int(win_size[0]), cell_size):
         grid_row=[]
         for j in range(0, int(win_size[0]), cell_size):
-            cell=Cell(i, j, cell_size, black, id, True, 0, 0, 0, (-1, -1), 1)
+            cell=Cell(i, j, cell_size, black, True, 0, 0, 0, (-1, -1), 1)
             grid_row.append(cell)
-            cell.draw(screen, 1)
-            id+=1
+            pygame.draw.rect(screen, cell.color, (cell.posx, cell.posy, cell.size, cell.size), 1)
         grid.append(grid_row)
-
 
     while True:
         for event in pygame.event.get():
@@ -155,22 +159,20 @@ def main():
         # set up start cell, end cell and walls
         if(pygame.mouse.get_pressed()[2]==1): #if wall
             wall_pos=prepareCell(grid, screen, black, (0, 0, 1))
-            walls_lst.append(wall_pos)
         elif(pygame.mouse.get_pressed()[0]==1 and se==1): #if start node
-            wall_pos=prepareCell(grid, screen, blue, (1, 0, 0))
+            wall_pos=prepareCell(grid, screen, green, (1, 0, 0))
             start_pos=wall_pos
             checkstart=pygame.mouse.get_pos()
             se=2
         elif(pygame.mouse.get_pressed()[0]==1 and se==2 and pygame.mouse.get_pos()!=checkstart): #if end node
-            wall_pos=prepareCell(grid, screen, red, (0, 1, 0))
+            wall_pos=prepareCell(grid, screen, green, (0, 1, 0))
             end_pos=wall_pos
             se=0
         #########################
         if(se==0):
             Astar(start_pos, end_pos ,grid, screen)
 
-        updateGrid(screen, grid, wall_pos)
-
+        updateGrid(screen, grid)
         pygame.time.Clock().tick(60)
         pygame.display.update()
 
